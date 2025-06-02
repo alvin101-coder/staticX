d84504e96b091efa885c1d46e7281c171bc24e72
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-06-02 18:54:26 +0300

    modified:   components/Avatar.js         modified:   components/Bulb.js         modified:   components/Circles.js         modified:   components/Header.js         modified:   components/Layout.js         modified:   components/Nav.js         modified:   components/ParticlesContainer.js         modified:   components/ProjectsBtn.js         modified:   components/ServiceSlider.js         modified:   components/Socials.js         modified:   components/TestimonialSlider.js         modified:   components/TopLeftImg.js         modified:   components/Transition.js         modified:   components/WorkSlider.js         new file:   package-lock.json         modified:   pages/_app.js         modified:   pages/about/index.js         modified:   pages/contact/index.js         modified:   pages/index.js         modified:   pages/services/index.js         modified:   pages/testimonials/index.js         modified:   pages/work/index.js         new file:   public/AJ-logo.png         modified:   public/logo.svg         new file:   public/logo1.png         new file:   public/logo2.svg         new file:   public/logo3.svg         new file:   public/logo4.svg

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,33 @@
+// links
+import Link from "next/link";
+
+// icons
+import {
+  RiInstagramLine,
+  RiLinkedinLine,
+  RiTwitterXLine,
+  RiGithubLine,
+} from 'react-icons/ri';
+
+const iconStyle = { fontSize: '25px' };
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+   <div className='flex items-center gap-x-5 text-lg'>
+     <Link href={'https://www.instagram.com/ajson_.dev?igsh=anN1ajM3OG1zajN3'} className="hover:text-accent transition-all duration-300">
+       <RiInstagramLine style={iconStyle} />
+     </Link>
+     <Link href={'https://www.linkedin.com/in/alvin-kanini-852665350?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'} className="hover:text-accent transition-all duration-300">
+       <RiLinkedinLine style={iconStyle} />
+     </Link>
+     <Link href={'https://x.com/AlvinKanini?t=YqVEFwSMG6hNh9NzfPOiqw&s=09'} className="hover:text-accent transition-all duration-300">
+       <RiTwitterXLine style={iconStyle} />
+     </Link>
+     <Link href={'https://github.com/alvin101-coder'} className="hover:text-accent transition-all duration-300">
+       <RiGithubLine style={iconStyle} />
+     </Link>
+  </div>
+  );
 };
 
 export default Socials;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,54 @@
+// components
+import WorkSlider from '../../components/WorkSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
+// framer motiom
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../variants'
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+  <div className='h-full bg-primary/30 py-36 flex items-center'>
+    <Circles />
+    <div className='container mx-auto'>
+      <div className='flex flex-col xl:flex-row gap-x-8'>
+        {/* text */}
+        <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+          <motion.h2
+           variants={fadeIn('up', 0.3)}
+           initial="hidden"
+           animate="show"
+           exit="hidden"
+           className='h2 xl:mt-12'
+           >
+            My Work <span className='text-accent'>.</span>
+            </motion.h2>
+            <motion.p 
+            variants={fadeIn('up', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+            >
+              I build solutions that merge functionality with intuitive design. Whether it's crafting dynamic UI components,troubleshooting complex layouts,or otimizing code for efficiency,every project is an opportunity to refine and innovate.My work isn't just about wriring code,it's about creating seamless, engaging digital experiences that solve real-world problems.Through clean syling,logical structuring, and interactive elements,I bring precision and creativity to modern web development.
+            </motion.p>
+        </div>
+        <motion.div
+        variants={fadeIn('down', 0.9)}
+        initial="hidden"
+        animate="show"
+        exit="hidden" 
+        className='w-full xl:max-w-[65%]'
+        >
+          {/* slider */}
+        <WorkSlider />
+        </motion.div>
+      </div>
+    </div>
+    <Bulb />
+  </div>
+  );
 };
 
 export default Work;
